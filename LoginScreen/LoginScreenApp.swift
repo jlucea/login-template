eabeaa20f33ff4a5c454ca3e6803d8f802da239c
Jaime Lucea <jaime.lucea@atactic.io>
2023-01-16 13:57:57 +0100

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Jaime Lucea on 16/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
